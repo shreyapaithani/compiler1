b0e8ce159090259a12b973a4178ad55c49bead85
Shreya Paithani <152017971+shreyapaithani@users.noreply.github.com>
2025-05-25 16:30:47 +0530

    Update semantic.py

--- semantic.py
+++ semantic.py
@@ -1,51 +1,86 @@
-from parser import parser
-from lexer import lexer
-from parser import Program, Function, Declaration, Return, BinOp, Number, Var
+import ply.yacc as yacc
+from lexer import tokens
 
-class SemanticAnalyzer:
-    def __init__(self):
-        self.symbol_table = {}
+# ---------- AST Node Classes ----------
+class Program:
+    def __init__(self, functions):
+        self.functions = functions
 
-    def analyze(self, node):
-        if isinstance(node, Program):
-            for fn in node.functions:
-                self.analyze(fn)
+class Function:
+    def __init__(self, name, body):
+        self.name = name
+        self.body = body
 
-        elif isinstance(node, Function):
-            print(f"Checking function: {node.name}")
-            self.symbol_table.clear()
-            for stmt in node.body:
-                self.analyze(stmt)
+class Declaration:
+    def __init__(self, var_type, var_name, value=None):
+        self.var_type = var_type
+        self.var_name = var_name
+        self.value = value
 
-        elif isinstance(node, Declaration):
-            if node.var_name in self.symbol_table:
-                raise Exception(f"Variable '{node.var_name}' already declared")
-            self.symbol_table[node.var_name] = node.var_type
-            self.analyze(node.value)
+class Return:
+    def __init__(self, value):
+        self.value = value
 
-        elif isinstance(node, Return):
-            self.analyze(node.value)
+class BinOp:
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
 
-        elif isinstance(node, BinOp):
-            self.analyze(node.left)
-            self.analyze(node.right)
+class Number:
+    def __init__(self, value):
+        self.value = value
 
-        elif isinstance(node, Number):
-            pass  # Numbers are valid
+class Var:
+    def __init__(self, name):
+        self.name = name
 
-        elif isinstance(node, Var):
-            if node.name not in self.symbol_table:
-                raise Exception(f"Variable '{node.name}' used before declaration")
+# ---------- Grammar Rules ----------
 
-# --------------- Test ----------------
-if __name__ == '__main__':
-    code = '''
-    int main() {
-        int a = 5;
-        return a + 10;
-    }
-    '''
-    ast = parser.parse(code, lexer=lexer)
-    analyzer = SemanticAnalyzer()
-    analyzer.analyze(ast)
-    print("Semantic Analysis Passed ✅")
+def p_program(p):
+    'program : function'
+    p[0] = Program([p[1]])
+
+def p_function(p):
+    'function : INT ID LPAREN RPAREN LBRACE statements RBRACE'
+    p[0] = Function(p[2], p[6])
+
+def p_statements_multiple(p):
+    'statements : statements statement'
+    p[0] = p[1] + [p[2]]
+
+def p_statements_single(p):
+    'statements : statement'
+    p[0] = [p[1]]
+
+def p_statement_declaration(p):
+    'statement : INT ID ASSIGN expression SEMICOLON'
+    p[0] = Declaration("int", p[2], p[4])
+
+def p_statement_return(p):
+    'statement : RETURN expression SEMICOLON'
+    p[0] = Return(p[2])
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression MULT expression
+                  | expression DIVIDE expression'''
+    p[0] = BinOp(p[1], p[2], p[3])
+
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = Number(p[1])
+
+def p_expression_var(p):
+    'expression : ID'
+    p[0] = Var(p[1])
+
+def p_error(p):
+    if p:
+        raise SyntaxError(f"Syntax error at '{p.value}' on line {p.lineno}")
+    else:
+        raise SyntaxError("Syntax error at EOF")
+
+# ---------- Build Parser ----------
+parser = yacc.yacc()
